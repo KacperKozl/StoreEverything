b5f43b78089f0b4bdbea807ec417caaedd7df5ab
KacperKozl <kacper.kozlowski.kontakt@gmail.com>
2023-06-06 15:17:50 +0200

    Poprawione sortowanie po kategorii

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,4 @@ VALUES (0,'gotowanie'),(1,'ważne'),(2,'przypominajki');
 INSERT INTO NOTES(title,content,link,add_date,reminder_date,category_id)
 VALUES('przepis','Żabie udka w sosie','www.wp.pl','2023-06-05','2023-06-07',(select CATEGORY_ID FROM CATEGORIES WHERE CATEGORIES.CATEGORY_NAME='gotowanie')),
 ('abrakdabra','czarna magia','www.onet.pl','2023-06-04','2023-06-06',(select CATEGORY_ID FROM CATEGORIES WHERE CATEGORIES.CATEGORY_NAME='ważne'));
+  
