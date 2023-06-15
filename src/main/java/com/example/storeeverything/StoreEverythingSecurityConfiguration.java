10e1e0a12d014151692b9ae7e97158c0dc2e60f7
wojtko <j.w2001@onet.pl>
2023-06-16 00:24:52 +0200

    Temporary SecurityConfiguration class setup

--- src/main/java/com/example/storeeverything/StoreEverythingSecurityConfiguration.java
+++ src/main/java/com/example/storeeverything/StoreEverythingSecurityConfiguration.java
@@ -1,9 +1,50 @@
 package com.example.storeeverything;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class StoreEverythingSecurityConfiguration extends WebSecurityConfigurerAdapter{
+abstract
+class StoreEverythingSecurityConfiguration {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withUsername("user1")
+                        .password("user1")
+                        .roles("USER")
+                        .build();
+
+        UserDetails admin =
+                User.withUsername("admin")
+                        .password("admin")
+                        .roles("ADMIN")
+                        .build();
+
+        System.out.println(user.getUsername()+"" + user.getPassword()+
+                "" +user.getAuthorities());
+
+        System.out.println(admin.getUsername()+"" + admin.getPassword()+
+                "" +admin.getAuthorities());
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .logout((logout) -> logout.logoutSuccessUrl("/").permitAll());
+
+        return http.build();
+    }
 
 }
