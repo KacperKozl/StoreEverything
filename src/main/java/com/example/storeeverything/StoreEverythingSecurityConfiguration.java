51a3195fa8e292b9aedd499c9a5a9f398b017a11
wojtko <j.w2001@onet.pl>
2023-06-15 23:57:08 +0200

    Create StoreEverythingSecurityConfiguration class

---
+++ src/main/java/com/example/storeeverything/StoreEverythingSecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.storeeverything;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class StoreEverythingSecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+}
