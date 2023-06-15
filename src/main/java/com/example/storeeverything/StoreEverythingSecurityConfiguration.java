6247b0f4af7c87d0bea9025d95b13d52dc55ab5a
wojtko <j.w2001@onet.pl>
2023-06-16 00:41:09 +0200

    Password encryption in the login controller

--- src/main/java/com/example/storeeverything/StoreEverythingSecurityConfiguration.java
+++ src/main/java/com/example/storeeverything/StoreEverythingSecurityConfiguration.java
@@ -3,15 +3,25 @@ package com.example.storeeverything;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-abstract
+@EnableWebSecurity
+//abstract
 class StoreEverythingSecurityConfiguration {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        int rounds = 10;
+        return new BCryptPasswordEncoder(rounds);
+    }
+
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user =
