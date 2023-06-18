0ba2ab5b325e591781783c3886c1cfdf19638bd9
wojtko <j.w2001@onet.pl>
2023-06-19 01:48:03 +0200

    Add edit user functionality. Fix some styling issue

--- src/main/java/com/example/storeeverything/controllers/UsersController.java
+++ src/main/java/com/example/storeeverything/controllers/UsersController.java
@@ -1,11 +1,19 @@
 package com.example.storeeverything.controllers;
 
+import com.example.storeeverything.data.Indeks;
+import com.example.storeeverything.data.Note;
+import com.example.storeeverything.data.User;
+import com.example.storeeverything.data.database.UsersEntity;
 import com.example.storeeverything.repositories.database.UsersEntityRepository;
 import com.example.storeeverything.services.dbService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UsersController {
@@ -18,6 +26,28 @@ public class UsersController {
     @GetMapping("/users")
     public String fetchUsers(Model model) {
         model.addAttribute("users",service.getUsersEntityRepository().findAll());
+        return "users";
+    }
+
+    @PostMapping("/users/edit")
+    public String showEditUser(Model model, @ModelAttribute("indeks") Indeks indeks) {
+        UsersEntity user = service.getUsersEntityRepository().findByUserId(indeks.getValue());
+        model.addAttribute("roles", service.getRolesEntityRepository().findAll());
+        model.addAttribute("editedUser", user);
+        model.addAttribute("newUser", new User());
+        return "edit_user";
+    }
+
+    @PostMapping("/user/edit/init")
+    public String editUser(@Valid @ModelAttribute("newUser") User newUser, BindingResult result, Model model) {
+       UsersEntity user = service.convertUser(newUser);
+        if(result.hasErrors()){
+            System.out.println(result.getAllErrors());
+            model.addAttribute("roles", service.getRolesEntityRepository().findAll());
+            model.addAttribute("editedUser", user);
+            return "edit_user";
+        }
+        service.getUsersEntityRepository().save(user);
         return "users";
     }
 }
