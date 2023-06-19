70ffe4d314c4629acf3bc63e4616e2cea36d314a
KacperKozl <kacper.kozlowski.kontakt@gmail.com>
2023-06-19 13:57:45 +0200

    Poprawa formatu daty w filtrowaniu

--- src/main/java/com/example/storeeverything/data/DateWrapper.java
+++ src/main/java/com/example/storeeverything/data/DateWrapper.java
@@ -4,9 +4,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.sql.Date;
+import java.text.SimpleDateFormat;
 
 @Data
 @NoArgsConstructor
 public class DateWrapper {
     Date data;
+    public String formatDate(Date data){
+        return new SimpleDateFormat("dd-MM-yyyy").format(data);
+    }
 }
